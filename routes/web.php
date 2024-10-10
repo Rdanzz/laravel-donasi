3969885898c1a20f5f4d352d2a594716b24cac00
Rdanzz <zidhanraffly123@gmail.com>
2024-10-10 19:10:26 +0700

    add laravel breeze

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,49 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\DonaturController;
+use App\Http\Controllers\FundraiserController;
+use App\Http\Controllers\FundraisingController;
+use App\Http\Controllers\FundraisingPhaseController;
+use App\Http\Controllers\FundraisingWithdrawController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('dashboard');
+    return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::prefix('admin')->name('admin')->group(function() {
+        Route::resource('categories', CategoryController::class)
+        ->middleware('role:owner');
+
+        Route::resource('donaturs', DonaturController::class)
+        ->middleware('role:owner');
+
+        Route::resource('fundraisers', FundraiserController::class)
+        ->middleware('role:owner')->except('index');
+
+        Route::get('fundraisers', [FundraiserController::class, 'index'])
+        ->name('fundraisers.index');
+
+        Route::resource('fundraising_withdraw', FundraisingWithdrawController::class)
+        ->middleware('role:owner|fundraiser');
+
+        Route::resource('fundraising_phases', FundraisingPhaseController::class)
+        ->middleware('role:owner|fundraiser');
+
+        Route::resource('fundraising', FundraisingController::class)
+        ->middleware('role:owner|fundraiser');
+    });
+});
+
+require __DIR__.'/auth.php';
