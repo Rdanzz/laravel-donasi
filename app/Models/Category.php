979fa89faabdae06a59376d3ad6aa390961bb825
Rdanzz <zidhanraffly123@gmail.com>
2024-10-10 15:36:24 +0700

    setup rlation ORM on model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -9,4 +9,8 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Category extends Model
 {
     use HasFactory, SoftDeletes;
+
+    public function fundraisings() {
+        return $this->hasMany(Fundraising::class);
+    }
 }
