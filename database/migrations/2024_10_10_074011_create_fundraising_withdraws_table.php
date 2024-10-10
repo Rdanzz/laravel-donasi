d6ab35340ee9d24c82fe940f7a86360727596218
Rdanzz <zidhanraffly123@gmail.com>
2024-10-10 14:41:01 +0700

    create model migration and controller

--- database/migrations/2024_10_10_074011_create_fundraising_withdraws_table.php
+++ database/migrations/2024_10_10_074011_create_fundraising_withdraws_table.php
@@ -11,11 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('categories', function (Blueprint $table) {
+        Schema::create('fundraising_withdraws', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('slug');
-            $table->string('icon');
             $table->timestamps();
         });
     }
@@ -25,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('categories');
+        Schema::dropIfExists('fundraising_withdraws');
     }
 };
