167beb727a76bae4c7c72da4d7156b63d50d822c
Rdanzz <zidhanraffly123@gmail.com>
2024-10-11 20:42:33 +0700

    edit delete category

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreCategoryRequest;
+use App\Http\Requests\UpdateCategoryRequest;
 use App\Models\Category;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -67,15 +68,29 @@ class CategoryController extends Controller
      */
     public function edit(Category $category)
     {
-        return view('admin.category.edit', compact('category'));
+        return view('admin.categories.edit', compact('category'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Category $category)
+    public function update(UpdateCategoryRequest $request, Category $category)
     {
-        //
+        DB::transaction(function () use ($request, $category) {
+            $validated = $request->validated();
+
+            if ($request->hasFile('icon')) {
+                $iconPath = $request->file('icon')->store('icons', 'public');
+                $validated['icon'] = $iconPath;
+            }
+
+            $validated['slug'] = Str::slug($validated['name']);
+
+            // dd($validated);
+
+            $category->update($validated);
+        });
+        return redirect()->route('admin.categories.index')->with('success', 'Categories success created.');
     }
 
     /**
@@ -83,6 +98,16 @@ class CategoryController extends Controller
      */
     public function destroy(Category $category)
     {
-        //
+        DB::beginTransaction();
+
+        try{
+            $category->delete();
+            DB::commit();
+            return redirect()->route('admin.categories.index')->with('success', 'Category success deleted.');
+        } 
+        catch (\Exception $e){
+            DB::rollBack();
+            return redirect()->route('admin.categories.index')->with('error', 'Failed to delete category.');
+        }
     }
 }
